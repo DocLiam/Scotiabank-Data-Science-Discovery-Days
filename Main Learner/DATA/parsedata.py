66808795da0c875b50e3754cc612f8db8c56e457
Liam Watson <LiamRWatson03@gmail.com>
2023-01-20 16:46:53 -0500

    update

--- Main Learner/DATA/parsedata.py
+++ Main Learner/DATA/parsedata.py
@@ -5,6 +5,17 @@ csv_filer = csv.DictReader(open("extracted_train_ScotiaDSD.csv"))
 i = 0
 
 for row in csv_filer:
+    MEAN_7DAY_values = []
+    STD_7DAY_values = []
+
+    MEAN_30DAY_values = []
+    STD_30DAY_values = []
     
+    for header in row:
+        MEAN_7DAY_values.append(float(row[header])) if header.startswith("MEAN") and header.endswith("7DAY") else None
+        STD_7DAY_values.append(float(row[header])) if header.startswith("STD") and header.endswith("7DAY") else None
+    
+        MEAN_30DAY_values.append(float(row[header])) if header.startswith("MEAN") and header.endswith("30DAY") else None
+        STD_30DAY_values.append(float(row[header])) if header.startswith("STD") and header.endswith("30DAY") else None
     
     i += 1
